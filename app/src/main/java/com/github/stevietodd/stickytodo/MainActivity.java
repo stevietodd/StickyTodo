39b0421a9628fededc969d4247eeb1c0c38316a1
Stevie Schraudner <tripletwollc@gmail.com>
2019-01-28 22:05:18 -0500

    Notifications working

--- app/src/main/java/com/github/stevietodd/stickytodo/MainActivity.java
+++ app/src/main/java/com/github/stevietodd/stickytodo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.github.stevietodd.stickytodo;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,78 +15,172 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+	MyRecyclerViewAdapter adapter;
+	List<String> tasks;
+	NotificationCompat.Builder notificationBuilder;
+	NotificationManagerCompat notificationManager;
 
-        createNotificationChannel();
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "StickyChannel")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("My notification")
-                .setContentText("Receipts\nLitter boxes\nTake out trash")
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText("Receipts\nLitter boxes\nTake out trash\nHow\nLow\nCan\nYou\nGo\n?"))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setOngoing(true);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(22, mBuilder.build());
-    }
+		tasks = new ArrayList<>();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+		// try to read in existing tasks from file
+		try {
+			FileInputStream fis = getBaseContext().openFileInput("tasks.txt");
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader bufferedReader = new BufferedReader(isr);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				tasks.add(line);
+			}
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+				alert.setTitle("Title");
+				alert.setMessage("Message");
 
-        return super.onOptionsItemSelected(item);
-    }
+				// Set an EditText view to get user input
+				final EditText input = new EditText(view.getContext());
+				alert.setView(input);
 
-    @TargetApi(23)
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("StickyChannel", "StickyChannel", importance);
-            channel.setDescription("StickyChannel");
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
+				alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						// Do something with value!
+						tasks.add(input.getText().toString());
+						notificationBuilder.setStyle(new NotificationCompat.BigTextStyle()
+								.bigText(String.join("\n", tasks)));
+						notificationManager.notify(22, notificationBuilder.build());
+					}
+				});
+
+				alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						dialog.cancel();
+					}
+				});
+
+				alert.show();
+			}
+		});
+
+		createNotificationChannel();
+		//TODO: need these 3 lines?
+		Intent intent = new Intent(this, MainActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+				PendingIntent.FLAG_ONE_SHOT);
+		notificationBuilder = new NotificationCompat.Builder(this, "StickyTodoChannel")
+				.setSmallIcon(R.mipmap.ic_launcher)
+				.setContentTitle("StickyTodo")
+				.setContentText("StickyTodo")
+				.setStyle(new NotificationCompat.BigTextStyle()
+						.bigText(String.join("\n", tasks)))
+				.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+				.setOngoing(true)
+				.setContentIntent(pendingIntent)
+				.setOnlyAlertOnce(true);
+
+		notificationManager = NotificationManagerCompat.from(this);
+		// notificationId is a unique int for each notification that you must define
+		notificationManager.notify(22, notificationBuilder.build());
+
+		// set up the RecyclerView
+		RecyclerView recyclerView = findViewById(R.id.myRecyclerView);
+		recyclerView.setLayoutManager(new LinearLayoutManager(this));
+		adapter = new MyRecyclerViewAdapter(this, tasks);
+		adapter.setClickListener(this);
+		recyclerView.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onItemClick(View view, int position) {
+		Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+	}
+
+	private void createNotificationChannel() {
+			int importance = NotificationManager.IMPORTANCE_DEFAULT;
+			NotificationChannel channel = new NotificationChannel("StickyTodoChannel", "StickyTodoChannel", importance);
+			channel.setDescription("StickyTodoChannel");
+			// Register the channel with the system; you can't change the importance
+			// or other notification behaviors after this
+			NotificationManager notificationManager = getSystemService(NotificationManager.class);
+			notificationManager.createNotificationChannel(channel);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		try {
+			FileOutputStream fos = getBaseContext().openFileOutput("tasks.txt", Context.MODE_PRIVATE);
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+			for (String task : tasks) {
+				bw.write(task);
+				bw.newLine();
+			}
+
+			bw.close();
+			fos.close();
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
+	}
 }
